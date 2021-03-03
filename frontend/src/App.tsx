0c330255b40e1548a095309272f1eb8389dc1bc4
Kilderson Sena <dersonsena@gmail.com>
2021-03-03 17:10:05 -0300

    some fix and improvements in eslint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css';
+import 'primeflex/primeflex.css';
+
+import { Menubar } from 'primereact/menubar';
+
+const menuItems = [{ label: 'Lançamentos' }, { label: 'Relatórios' }];
 
 const App = () => (
   <div className="App">
+    <Menubar model={menuItems} />
     <h1>Control Savings App !!</h1>
   </div>
 );
